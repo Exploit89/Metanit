b293507ea497c15eeb7eadffefad9a6ee7f05c07
Exploit89 <khudajbergenov89@gmail.com>
2022-07-06 12:20:00 +1000

    PC_Work commit

---
+++ ClassesAndObjects/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    class Person
+    {
+        public const string type = "Person";
+        public string name = "Undefined";
+        public int age;
+        public void Print() => Console.WriteLine($"{type}: {name} - {age}");
+    }
+}
--- ClassesAndObjects/Program.cs
+++ ClassesAndObjects/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClassesAndObjects
 {
@@ -10,6 +6,21 @@ namespace ClassesAndObjects
     {
         static void Main(string[] args)
         {
+            Person tom = new Person();  // создание объекта класса Person
+
+            // Получаем значение полей в переменные
+            string personName = tom.name;
+            int personAge = tom.age;
+            Console.WriteLine($"Имя: {personName}  Возраст {personAge}");   // Имя: Undefined  Возраст: 0
+
+            // устанавливаем новые значения полей
+            tom.name = "Tom";
+            tom.age = 37;
+
+            // обращаемся к методу Print
+            tom.Print();    // Имя: Tom  Возраст: 37
+            Console.WriteLine(Person.type); // Person
+            // Person.type = "User";    // !Ошибка: изменить константу нельзя
         }
     }
 }
